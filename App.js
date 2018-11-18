94bb9caa66fc5ab8b07efb90a6ce7656ed5bce2e
krishna kumar <krishnau2@gmail.com>
2018-11-18 23:45:35 +0530

    Moved data-initialisation to it's own file.

--- App.js
+++ App.js
@@ -6,16 +6,15 @@ import {
   StatusBar,
   AsyncStorage
 } from 'react-native';
-import {
-  AppNavigator
-} from './navigator';
+import { AppNavigator } from './navigator';
+import { InitializeAppData } from './AppInitializer'
 
 const CURRENT_YEAR = '2018';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.initializeAsyncStorage();
+    InitializeAppData();
     // this.clearData();
   }
 
@@ -28,43 +27,9 @@ export default class App extends React.Component {
     );
   }
 
-  initializeAsyncStorage() {
-    this.getAsyncData('initialized').then((data) => {
-      if(data && data === true){
-        // Don't overwrite the async data
-      }else{
-        // Initialize Year, month - empty data
-        AsyncStorage.setItem("initialized", JSON.stringify(true));
-        AsyncStorage.setItem(CURRENT_YEAR, JSON.stringify(this.generateInitialData()));
-      }
-    });
-  }
-
-  generateInitialData() {
-    let initialData = [];
-    for(var i = 1; i <= 12; i++){
-      initialData.push({
-        month: i < 10 ? '0'+i : ''+i ,
-        data: []
-      });
-    }
-    return initialData;
-  }
-
   clearData() {
     AsyncStorage.setItem(CURRENT_YEAR, JSON.stringify([]));
-  }
-
-  async getAsyncData (asyncKey) {
-    try {
-      const result = await AsyncStorage.getItem(asyncKey);
-      const item = JSON.parse(result);
-      return item;
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  
+  }  
 }
 
 const styles = StyleSheet.create({
---
+++ AppInitializer.js
@@ -0,0 +1,38 @@
+import {
+  AsyncStorage
+} from 'react-native';
+
+const CURRENT_YEAR = '2018';
+
+async function readAsyncData (asyncKey) {
+    try {
+      const result = await AsyncStorage.getItem(asyncKey);
+      const item = JSON.parse(result);
+      return item;
+    } catch (error) {
+      console.log(error)
+    }
+}
+
+function generateInitialData() {
+    let initialData = [];
+    for(var i = 1; i <= 12; i++){
+        initialData.push({
+            month: i < 10 ? '0'+i : ''+i ,
+            data: []
+        });
+    }
+    return initialData;
+}
+
+export const InitializeAppData = () => {
+    readAsyncData('initialized').then((data) => {
+        if(data && data === true){
+            // Don't overwrite the async data
+        }else{
+            // Initialize Year, month - empty data
+            AsyncStorage.setItem("initialized", JSON.stringify(true));
+            AsyncStorage.setItem(CURRENT_YEAR, JSON.stringify(generateInitialData()));
+        }
+    });
+}
